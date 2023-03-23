acc57de96bfe5519ef5410b2e594a6dcaf7dbf12
Suryakanta Das <116926717+surya-situ@users.noreply.github.com>
2023-03-23 21:02:04 +0530

    nested comments

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import {  useSearchParams } from 'react-router-dom';
 import { closeMenu } from '../utils/appSlice';
+import CommentContainer from './CommentContainer';
 
 const WatchPage = () => {
 
@@ -14,17 +15,22 @@ const WatchPage = () => {
     },[]);
 
     // https://www.youtube.com/watch?v=LNUiseO9nH8
+    
   return (
-    <div className='px-5'>
-      <iframe 
-        width="1200" height="600" 
-        // this link is form youtube embedded link from video share
-        src={"https://www.youtube.com/embed/" + searchParams.get("v")} title="YouTube video player" 
-        frameborder="0" 
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-        allowfullscreen>
-      </iframe>
+    <div className='flex flex-col'>
+      <div className='px-5'>
+        <iframe 
+          width="1200" height="600" 
+          // this link is form youtube embedded link from video share: {https://www.youtube.com/watch?v=LNUiseO9nH8 }
+          src={"https://www.youtube.com/embed/" + searchParams.get("v")} title="YouTube video player" 
+          frameBorder="0" 
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+          allowFullScreen>
+        </iframe>
+      </div>
+      <CommentContainer />
     </div>
+    
   )
 }
 
