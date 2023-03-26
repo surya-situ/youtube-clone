d05c73399beabd85c1ab99d3de314975058076e8
Suryakanta Das <116926717+surya-situ@users.noreply.github.com>
2023-03-26 20:27:49 +0530

    live caht feature

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -3,6 +3,7 @@ import { useDispatch } from 'react-redux'
 import {  useSearchParams } from 'react-router-dom';
 import { closeMenu } from '../utils/appSlice';
 import CommentContainer from './CommentContainer';
+import LiveChat from './LiveChat';
 
 const WatchPage = () => {
 
@@ -15,18 +16,23 @@ const WatchPage = () => {
     },[]);
 
     // https://www.youtube.com/watch?v=LNUiseO9nH8
-    
+
   return (
-    <div className='flex flex-col'>
-      <div className='px-5'>
-        <iframe 
-          width="1200" height="600" 
-          // this link is form youtube embedded link from video share: {https://www.youtube.com/watch?v=LNUiseO9nH8 }
-          src={"https://www.youtube.com/embed/" + searchParams.get("v")} title="YouTube video player" 
-          frameBorder="0" 
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-          allowFullScreen>
-        </iframe>
+    <div className='flex flex-col w-full'>
+      <div className='flex px-5'>
+        <div>
+          <iframe 
+            width="1100" height="550" 
+            // this link is form youtube embedded link from video share: {https://www.youtube.com/watch?v=LNUiseO9nH8 }
+            src={"https://www.youtube.com/embed/" + searchParams.get("v")} title="YouTube video player" 
+            frameBorder="0" 
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+            allowFullScreen>
+          </iframe>
+        </div>
+        <div className='w-full'>
+          <LiveChat />
+        </div>
       </div>
       <CommentContainer />
     </div>
