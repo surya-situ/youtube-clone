8fef899520458509079c0a78cf2f2cad7cb417bc
Suryakanta Das <116926717+surya-situ@users.noreply.github.com>
2023-03-26 20:30:21 +0530

    new feature

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import store from "./utils/store"
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import MainContainer from "./components/MainContainer";
 import WatchPage from "./components/WatchPage";
+import Demo from "./components/Demo";
 
 const appRouter = createBrowserRouter([{
   path: "/",
@@ -18,6 +19,10 @@ const appRouter = createBrowserRouter([{
     {
       path:'watch',
       element: <WatchPage />
+    },
+    {
+      path:'demo',
+      element:<Demo/>
     }
   ]
 }])
